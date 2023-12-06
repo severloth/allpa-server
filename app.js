dbbcc0fc8b2de333b332fee7f5b3e337242fea50
Facundo Martin Gil <u59448@msm.gov.ar>
2023-12-05 23:32:38 -0300

    Changes to createImage

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(cors())
 
-//app.use(express.static("__dirname" + "/uploads"));
+app.use(express.static(__dirname + "/uploads"));
 
 
 // Routes
