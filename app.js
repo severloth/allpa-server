178a8f62891b18979514149e933867c023301326
Facundo Martin Gil <u59448@msm.gov.ar>
2023-12-07 11:49:14 -0300

    nodemailer

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const cookieParser = require("cookie-parser");
+const nodemailer = require("nodemailer");
 
 const user = require("./Routes/user.route");
 const app = express();
 const dotenv = require("dotenv");
 
 dotenv.config();
+
+
 
 // Settings
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -21,7 +24,48 @@ app.use(express.static(__dirname + "/uploads"));
 // Routes
 app.use("/", user);
 
+const enviarMail = async (nombre, email, consulta) => {
+    const config = {
+      host: "smtp.gmail.com",
+      port: 587,
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    };
+  
+    const htmlBody = `
+    <p><strong>Nueva consulta de usuario:</strong></p>
+    <p><strong>Nombre:</strong> ${nombre}</p>
+    <p><strong>Email:</strong> ${email}</p>
+    <p><strong>Consulta:</strong></p>
+    <p>${consulta}</p>
+  `;
 
-
+  const mail = {
+    from: "moonit.mail@gmail.com",
+    to: process.env.TO_EMAIL,
+    subject: "Nueva consulta de usuario",
+    html: htmlBody,
+  };
+  
+    const transporter = nodemailer.createTransport(config);
+  
+    const info = await transporter.sendMail(mail);
+    console.log(info);
+  };
+  
+  app.post("/enviar-consulta", (req, res) => {
+    const { nombre, email, consulta } = req.body;
+  
+    enviarMail(nombre, email, consulta)
+    .then(() => {
+      res.status(200).json({ status: 'success', message: 'Correo enviado con éxito' });
+    })
+    .catch((error) => {
+      console.error("Error al enviar el correo:", error);
+      res.status(500).json({ status: 'error', message: error.toString() });
+    });
+  });
 
 module.exports = app;
